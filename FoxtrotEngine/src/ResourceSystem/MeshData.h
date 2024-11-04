a528c19402fd51bc33133ef6489f5ff3beb0732b
Joseph Park Dev <human1654@gmail.com>
2024-11-04 13:54:32 +0900

    COMMIT Added MeshData::IsEmpty()

--- FoxtrotEngine/src/ResourceSystem/MeshData.h
+++ FoxtrotEngine/src/ResourceSystem/MeshData.h
@@ -6,4 +6,8 @@
 struct MeshData {
     std::vector<Vertex>   vertices;
     std::vector<uint32_t> indices; // uint32·Î º¯°æ
+
+    bool IsEmpty() const {
+        return vertices.empty() || indices.empty();
+    }
 };

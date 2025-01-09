8cec09214c1ef2d8e05ff3ff210b0b6988f4e443
Joseph Park Dev <human1654@gmail.com>
2025-01-09 15:51:54 +0900

    COMMIT Revised Comments for BinarySearch() & InsertionSort()

--- FoxtrotDS/SearchFuncs.h
+++ FoxtrotDS/SearchFuncs.h
@@ -5,6 +5,15 @@
 
 namespace FTDS
 {
+	/// <summary>
+	/// Binary Search for an array, working only for arrays with numerical values.
+	/// Returns -1 if the target value is not found.
+	/// </summary>
+	/// <typeparam name="NUM : ">Type for the numerical value.</typeparam>
+	/// <param name="array : ">An array.</param>
+	/// <param name="count : ">The length of an array.</param>
+	/// <param name="target : ">Value to search for.</param>
+	/// <returns></returns>
 	template <typename NUM, typename std::enable_if<std::is_arithmetic<NUM>::value>::type* = nullptr>
 	int BinarySearch(NUM* array, size_t count, NUM target)
 	{

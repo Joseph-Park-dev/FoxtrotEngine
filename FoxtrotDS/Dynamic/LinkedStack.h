496769dfe71709b02314b5fcb4073209d9846818
jbpark1654 <jbpark1654@gmail.com>
2025-01-24 10:29:16 +0900

    COMMIT Added LinkedStack

---
+++ FoxtrotDS/Dynamic/LinkedStack.h
@@ -0,0 +1,51 @@
+#pragma once
+#include "LinkedList.h"
+
+namespace FTDS
+{
+    template <typename TYPE>
+    class LinkedStack :
+        public FTDS::LinkedList<TYPE>
+    {
+        using LinkedList<TYPE>::mHead;
+        using LinkedList<TYPE>::mSize;
+
+    public:
+        virtual void Push(TYPE val) override
+        {
+            Node<TYPE>* node = DBG_NEW Node<TYPE>(val);
+            if(!this->IsEmpty())
+            {
+                assert(mHead);
+                node->Next = mHead;
+            }
+            mHead = node;
+            ++mSize;
+        }
+
+        virtual void Pop() override
+        {
+            if (mHead)
+            {
+                Node<TYPE>* last = mHead;
+                mHead = mHead->Next;
+                delete last;
+                last = nullptr;
+                --mSize;
+            }
+        }
+
+        TYPE Peek() { return mHead->Value; }
+
+    public:
+        LinkedStack()
+            : LinkedList<TYPE>()
+        { }
+
+        ~LinkedStack()
+        {
+            while (!this->IsEmpty())
+                Pop();
+        }
+    };
+}

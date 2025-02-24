d75df5a1eeeb0ec12b7cc3345812b0ceabec27cc
Joseph Park Dev <human1654@gmail.com>
2025-02-24 17:19:13 +0900

    COMMIT Divided ChunkFileKeys into the ones containing relevant keys

---
+++ FoxtrotEngine/src/FileSystem/BufferSizes.h
@@ -0,0 +1,16 @@
+// ----------------------------------------------------------------
+// Foxtrot Engine 2D
+// Copyright (C) 2025 JungBae Park. All rights reserved.
+//
+// Released under the GNU General Public License v3.0
+// See LICENSE in root directory for full details.
+// ----------------------------------------------------------------
+/// <summary>
+/// Various buffer sizes by data types.
+/// </summary>
+/// 
+#pragma once
+namespace BufferSizes
+{
+	constexpr size_t MAX_STRING_SIZE = 50;
+}
---
+++ FoxtrotEngine/src/FileSystem/FileTypes.h
@@ -0,0 +1,23 @@
+// ----------------------------------------------------------------
+// Foxtrot Engine 2D
+// Copyright (C) 2025 JungBae Park. All rights reserved.
+//
+// Released under the GNU General Public License v3.0
+// See LICENSE in root directory for full details.
+// ----------------------------------------------------------------
+/// <summary>
+/// File types (extensions) that are used in Foxtrot Engine.
+/// </summary>
+
+#pragma once
+namespace FileTypes
+{
+	// Essential Files
+	constexpr const char* CHUNK = ".chunk";
+	constexpr const char* GDPACK = ".gdpack";
+
+	// FTResources
+	constexpr const char* PREMADE = ".premade";
+	constexpr const char* TEXTURE = ".png, .jpeg, .jpg";
+	constexpr const char* TILEMAP = ".csv, .txt";
+}

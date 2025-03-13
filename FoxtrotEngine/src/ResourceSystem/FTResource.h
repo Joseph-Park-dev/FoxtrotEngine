a1239378fa8913306c776ffb1043d7759c402b0c
Joseph Park Dev <human1654@gmail.com>
2025-03-13 13:50:56 +0900

    COMMIT FTResource Getters (filename, relative path) are now const

--- FoxtrotEngine/src/ResourceSystem/FTResource.h
+++ FoxtrotEngine/src/ResourceSystem/FTResource.h
@@ -1,7 +1,7 @@
 // ----------------------------------------------------------------
 // Foxtrot Engine 2D
 // Copyright (C) 2025 JungBae Park. All rights reserved.
-// 
+//
 // Released under the GNU General Public License v3.0
 // See LICENSE in root directory for full details.
 // ----------------------------------------------------------------
@@ -16,30 +16,30 @@
 class FTResource
 {
 public:
-	std::string&  GetFileName() { return mFileName; }
-	std::string&  GetRelativePath(){ return mRelativePath; }
-	UINT		  GetRefCount() { return mRefCount; }
-	bool		  GetIsProcessed() { return mIsProcessed; }
+	const std::string& GetFileName() { return mFileName; }
+	const std::string& GetRelativePath() { return mRelativePath; }
+	UINT			   GetRefCount() { return mRefCount; }
+	bool			   GetIsProcessed() { return mIsProcessed; }
 
-	void		  SetFileName(const std::string name) { mFileName.assign(name); }
-	void		  SetRelativePath(const std::string _strPath) { mRelativePath.assign(_strPath); }
-	void		  SetIsReferenced(bool val) { mRefCount = val; }
-	void		  SetIsProcessed(bool val) { mIsProcessed = val; }
+	void SetFileName(const std::string name) { mFileName.assign(name); }
+	void SetRelativePath(const std::string _strPath) { mRelativePath.assign(_strPath); }
+	void SetIsReferenced(bool val) { mRefCount = val; }
+	void SetIsProcessed(bool val) { mIsProcessed = val; }
 
-	void		  AddRefCount() { ++mRefCount; }
-	void		  SubtractRefCount() { --mRefCount; }
+	void AddRefCount() { ++mRefCount; }
+	void SubtractRefCount() { --mRefCount; }
 
 public:
 	FTResource();
-	virtual ~FTResource(){}
+	virtual ~FTResource() {}
 
 private:
 	std::string mFileName;
 	std::string mRelativePath;
 
 	// The resource is used in somewhere.
-	UINT		mRefCount;
-	bool		mIsProcessed;
+	UINT mRefCount;
+	bool mIsProcessed;
 
 public:
 	virtual void SaveProperties(std::ofstream& ofs, UINT key);
@@ -56,7 +56,7 @@ protected:
 
 namespace ChunkKey
 {
-	constexpr const char* KEY = "Key";
-	constexpr const char* FILE_NAME = "FileName";
+	constexpr const char* KEY			= "Key";
+	constexpr const char* FILE_NAME		= "FileName";
 	constexpr const char* RELATIVE_PATH = "RelativePath";
-}
+} // namespace ChunkKey

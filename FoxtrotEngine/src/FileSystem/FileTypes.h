66c7d7d6713ea345749279de910dd4ed47555e44
Joseph Park Dev <human1654@gmail.com>
2025-03-17 17:40:22 +0900

    COMMIT Added .stl filter to FileTypes::MESH

--- FoxtrotEngine/src/FileSystem/FileTypes.h
+++ FoxtrotEngine/src/FileSystem/FileTypes.h
@@ -20,7 +20,7 @@ namespace FileTypes
 	constexpr const char* PREMADE = ".ftpremade";
 	constexpr const char* TEXTURE = ".png, .jpeg, .jpg";
 	constexpr const char* TILEMAP = ".csv, .txt";
-	constexpr const char* MESH = ".fbx";
+	constexpr const char* MESH = ".fbx, .stl";
 	constexpr const char* SHADER = ".hlsl";
 	constexpr const char* MATERIAL = ".ftmat";
 

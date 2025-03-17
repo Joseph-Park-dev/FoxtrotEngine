f21c6fb3c1a3488c12a1c8587244ce1d267bdb2d
Joseph Park Dev <human1654@gmail.com>
2025-03-17 13:58:19 +0900

    COMMIT Implemented save to file feature: StandardMaterial

--- FoxtrotEngine/src/ResourceSystem/FTMaterials/StandardMaterial.h
+++ FoxtrotEngine/src/ResourceSystem/FTMaterials/StandardMaterial.h
@@ -13,9 +13,11 @@ class StandardMaterial :
 	public FTMaterial
 {
 public:
-	//void AssignData(StandardMatData& standardDest, BlinnPhongData& blinnPhongDest);
+	// void AssignData(StandardMatData& standardDest, BlinnPhongData& blinnPhongDest);
 	virtual void CreatePixelConstBuffer(ComPtr<ID3D11Device>& device, ComPtr<ID3D11Buffer>& buffer) override;
 	virtual void UpdateBuffer(ComPtr<ID3D11DeviceContext>& context, ComPtr<ID3D11Buffer>& buffer) override;
+
+	virtual void LoadFromFile() override;
 
 public:
 	StandardMaterial();
@@ -24,8 +26,14 @@ public:
 private:
 	StandardMatData* mData;
 
+public:
+	virtual void SaveProperties(std::ofstream& ofs, UINT key);
+	virtual UINT LoadProperties(std::ifstream& ifs);
+
 #ifdef FOXTROT_EDITOR
 public:
+	virtual void SaveToFile() override;
+	
 	void UpdateUI() override;
 
 #endif
@@ -33,9 +41,9 @@ public:
 
 struct BlinnPhongData
 {
-	FTVector3 Ambient = FTVector3(0.5f);
+	FTVector3 Ambient	= FTVector3(0.5f);
 	float	  Shininess = 0.5f;
-	FTVector3 Diffuse = FTVector3(0.5f);
+	FTVector3 Diffuse	= FTVector3(0.5f);
 	float	  dummy1;
 	FTVector3 Specular = FTVector3(0.5f);
 	float	  dummy2;
@@ -46,22 +54,22 @@ struct StandardMatData
 	Vector3		   EyeWorld;
 	uint32_t	   UseTexture;
 	Light		   Lights[GameData::MAX_LIGHTS];
-	Vector4		   IndexColor;
 	BlinnPhongData BlinnPhongData;
 };
 
 namespace ChunkKey
 {
+	constexpr const char* STANDARD_MAT = "StandardMaterial";
 	constexpr const char* USE_TEXTURE = "Use Texture";
 
 	namespace BlinnPhong
 	{
-		constexpr const char* AMBIENT = "Ambient";
+		constexpr const char* AMBIENT	= "Ambient";
 		constexpr const char* SHININESS = "Shininess";
-		constexpr const char* DIFFUSE = "Diffuse";
-		constexpr const char* SPECULAR = "Specular";
-	}
+		constexpr const char* DIFFUSE	= "Diffuse";
+		constexpr const char* SPECULAR	= "Specular";
+	} // namespace BlinnPhong
 	constexpr const unsigned int BASIC_MATERIAL = 1;
-}
+} // namespace ChunkKey
 
 static_assert((sizeof(StandardMatData) % 16) == 0, "Constant Buffer size must be 16-byte aligned");

75bb7dcda4e206ba6feabadb1a1fd5d9ae951a32
Joseph Park Dev <human1654@gmail.com>
2025-04-10 17:42:46 +0900

    COMMIT Added FTSpriteSheet

---
+++ FoxtrotEngine/src/ResourceSystem/FTSpriteSheet.cpp
@@ -0,0 +1,115 @@
+#include "FTSpriteSheet.h"
+
+#include "Math/FTMath.h"
+
+#include "Renderer/FTRectArea.h"
+#include "ResourceSystem/Tile.h"
+#include "ResourceSystem/GenericData/FTJSON.h"
+#include "FileSystem/NullKeys.h"
+#include "FileSystem/FileIOHelper.h"
+#include "Managers/ResourceManager.h"
+
+#ifdef FOXTROT_EDITOR
+#include "EditorUtils.h"
+#endif // FOXTROT_EDITOR
+
+
+void FTSpriteSheet::Initialize()
+{
+	if (mJSONKey == ChunkKey::NullVal::VALUE_NOT_ASSIGNED)
+		return;
+	FTJSON* json = ResourceManager::GetInstance()->GetLoadedJSON(mJSONKey);
+
+	InitializeProperties(json->Data().at(SpriteSheetKeys::PROPERTIES));
+
+	size_t tileCount = json->Data().at(SpriteSheetKeys::BASE).size();
+	mTiles = DBG_NEW Tile[tileCount];
+
+	size_t i = 0;
+	for (auto& item : json->Data().at(SpriteSheetKeys::BASE))
+	{
+		if (item.is_null())
+			return;
+		InitializeTile(mTiles[i], item);
+		++i;
+	}
+}
+
+Tile* FTSpriteSheet::GetTiles()
+{
+	return mTiles;
+}
+
+FTSpriteSheet::FTSpriteSheet()
+	: mJSONKey(ChunkKey::NullVal::VALUE_NOT_ASSIGNED)
+	, mTiles(nullptr)
+	, mSheetSize(FTVector2::Zero)
+{
+}
+
+FTSpriteSheet::~FTSpriteSheet()
+{
+	delete[] mTiles;
+}
+
+void FTSpriteSheet::InitializeTile(Tile& tile, nlohmann::json& json)
+{
+	float posOnMapX = json[SpriteSheetKeys::FRAME][SpriteSheetKeys::X] / mSheetSize.x;
+	float posOnMapY = json[SpriteSheetKeys::FRAME][SpriteSheetKeys::Y] / mSheetSize.y;
+	float width		= json[SpriteSheetKeys::FRAME][SpriteSheetKeys::W] / mSheetSize.x;
+	float height	= json[SpriteSheetKeys::FRAME][SpriteSheetKeys::H] / mSheetSize.y;
+
+	InitializeRectOnMap(tile, posOnMapX, posOnMapY, width, height);
+}
+
+void FTSpriteSheet::SaveProperties(std::ofstream& ofs, UINT key)
+{
+	FileIOHelper::BeginDataPackSave(ofs, ChunkKey::SpriteSheet::SPRITE_SHEET);
+	FTResource::SaveProperties(ofs, key);
+	FileIOHelper::SaveUnsignedInt(ofs, ChunkKey::SpriteSheet::JSON_KEY, mJSONKey);
+	FileIOHelper::SaveVector2(ofs, ChunkKey::SpriteSheet::SHEET_SIZE, mSheetSize);
+	FileIOHelper::EndDataPackSave(ofs, ChunkKey::SpriteSheet::SPRITE_SHEET);
+}
+
+UINT FTSpriteSheet::LoadProperties(std::ifstream& ifs)
+{
+	FileIOHelper::BeginDataPackLoad(ifs, ChunkKey::SpriteSheet::SPRITE_SHEET);
+	FileIOHelper::LoadVector2(ifs, mSheetSize);
+	FileIOHelper::LoadUnsignedInt(ifs, mJSONKey);
+	return FTResource::LoadProperties(ifs);
+}
+
+void FTSpriteSheet::InitializeProperties(nlohmann::json& json)
+{
+	float width	 = json.at(SpriteSheetKeys::SIZE).at(SpriteSheetKeys::W);
+	float height = json.at(SpriteSheetKeys::SIZE).at(SpriteSheetKeys::H);
+	mSheetSize	 = FTVector2(width, height);
+}
+
+void FTSpriteSheet::InitializeRectOnMap(Tile& tile, float posX, float posY, float width, float height)
+{
+	FTRectArea& rectOnMap = tile.GetRectOnMap();
+	tile.GetRectOnMap().Set(posX, posY, width, height);
+}
+
+void FTSpriteSheet::InitializeRectOnScreen(Tile& tile, float posOnMapX, float posOnMapY, float widthOnMap, float heightOnMap)
+{
+	FTRectArea& rectOnScreen = tile.GetRectOnScreen();
+}
+
+void FTSpriteSheet::UpdateUI()
+{
+	ImVec2 previewSize = ImVec2(100, 100);
+
+	CommandHistory::GetInstance()->UpdateStringValue("Sheet Name", FileName());
+
+	FTEditorUtils::DisplayResSelection(
+		"Select JSON",
+		ResourceManager::GetInstance()->GetMapJSONs(),
+		mJSONKey);
+
+	std::string text = { "Sheet size : " };
+	text += std::to_string(mSheetSize.x) + " ,";
+	text += std::to_string(mSheetSize.y);
+	ImGui::Text(text.c_str());
+}

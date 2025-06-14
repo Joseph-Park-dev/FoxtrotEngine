9be79ad7f6278a361b3346e5c17cbe11f5a89860
Joseph Park Dev <human1654@gmail.com>
2025-06-14 11:30:07 +0900

    COMMIT Added Geometry Shader Constant BufferData

--- FoxtrotEngine/src/ResourceSystem/MeshConstantData.h
+++ FoxtrotEngine/src/ResourceSystem/MeshConstantData.h
@@ -33,6 +33,15 @@ struct BasicVCData
 };
 static_assert((sizeof(BasicVCData) % 16) == 0, "Constant Buffer size must be 16-byte aligned");
 
+struct GSCBufferData
+{
+	Vector2 size;
+	float outlineWidth;
+	float dummy;
+};
+
+static_assert((sizeof(GSCBufferData) % 16) == 0, "Constant Buffer size must be 16-byte aligned");
+
 struct NormalVCData
 {
 	float scale = 0.1f;

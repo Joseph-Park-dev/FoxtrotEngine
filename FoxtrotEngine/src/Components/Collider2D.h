30d1ce615439c90456f66469c354aca0d83b4393
Joseph Park Dev <human1654@gmail.com>
2025-06-19 14:18:59 +0900

    COMMIT Added Collider2D::UpdateDebugShape()

--- FoxtrotEngine/src/Components/Collider2D.h
+++ FoxtrotEngine/src/Components/Collider2D.h
@@ -79,6 +79,9 @@ private:
 
 	CollisionStatesMap mCollisionStates;
 
+protected:
+	virtual void UpdateDebugShape(Camera* camInst) = 0;
+
 public:
 	virtual void SaveProperties(std::ofstream& ofs) override;
 	virtual void LoadProperties(std::ifstream& ifs) override;

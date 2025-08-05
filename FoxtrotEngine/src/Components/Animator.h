fb0c355c5a835eed2ad594d2a5753f2c334b7009
Joseph Park Dev <human1654@gmail.com>
2025-08-05 14:42:32 +0900

    COMMIT Removed adding resource reference count feature from Animator component

--- FoxtrotEngine/src/Components/Animator.h
+++ FoxtrotEngine/src/Components/Animator.h
@@ -78,9 +78,6 @@ public:
 	virtual void EditorUIUpdate() override;
 	void		 EditorRender(FoxtrotRenderer* renderer) override;
 
-public:
-	virtual void AddResRefCount() override;
-
 private:
 	void UpdatePlayAnim();
 	void UpdatePlayList();

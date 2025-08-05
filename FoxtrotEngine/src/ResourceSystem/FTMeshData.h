7ad670538dc53605264058b5117348df96c2182b
Joseph Park Dev <human1654@gmail.com>
2025-08-05 14:52:07 +0900

    COMMIT Removed IndexedMeshData

--- FoxtrotEngine/src/ResourceSystem/FTMeshData.h
+++ FoxtrotEngine/src/ResourceSystem/FTMeshData.h
@@ -25,14 +25,6 @@ struct FTMeshData
 	std::vector<uint32_t> Indices;
 };
 
-struct FTIndexedMeshData
-{
-	bool IsEmpty() const;
-
-	std::vector<IndexedVertex> Vertices;
-	std::vector<uint32_t>	   Indices;
-};
-
 struct FTDebugMeshData
 {
 	bool IsEmpty() const;

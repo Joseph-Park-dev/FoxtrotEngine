c693e77544cbb85baddbf150d722d8a838d472f6
Joseph Park Dev <human1654@gmail.com>
2025-09-02 14:56:00 +0900

    COMMIT FTMaterial: Added comments & initialization struct

--- FoxtrotEngine/src/ResourceSystem/FTMaterials/FTMaterial.cpp
+++ FoxtrotEngine/src/ResourceSystem/FTMaterials/FTMaterial.cpp
@@ -4,21 +4,33 @@
 #include <Renderer/D3D11Utils.h>
 #include <Renderer/FoxtrotRenderer.h>
 
-void FTMaterial::Process(FTCore* coreInst)
+void FTMaterial::SaveProperties(std::ofstream& ofs)
 {
-	if (this->GetIsProcessed())
-		return;
+}
 
-	this->LoadFromFile();
-
-	FoxtrotRenderer* renderer = coreInst->GetGameRenderer();
-	CreatePixelConstBuffer(renderer->GetDevice());
-
-	// All loaded premades are included as default.
-	this->SetIsProcessed(true);
+void FTMaterial::LoadProperties(std::ifstream& ifs)
+{
 }
 
 ComPtr<ID3D11Buffer>& FTMaterial::GetPCBuf()
 {
 	return mPCBuf;
+}
+
+FTMaterial::FTMaterial(FTResourceDef& resDef, FoxtrotRenderer* renderer)
+	: FTResource(resDef)
+{
+	Process(renderer);
+}
+
+void FTMaterial::Process(FoxtrotRenderer* renderer)
+{
+	if (this->IsProcessed())
+		return;
+
+	std::ifstream ifs(GetRelativePath().C_Str());
+	this->LoadProperties(ifs);
+
+	CreatePixelConstBuffer(renderer->GetDevice());
+	FTResource::Process();
 }

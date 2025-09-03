7b78b52b80c1e9a5f6a476c9beefc894b28544bf
Joseph Park Dev <human1654@gmail.com>
2025-09-03 10:42:18 +0900

    COMMIT FTVertexShader: Added comment, removed unnecessary member functions

--- FoxtrotEngine/src/ResourceSystem/FTShaders/FTVertexShader.cpp
+++ FoxtrotEngine/src/ResourceSystem/FTShaders/FTVertexShader.cpp
@@ -10,7 +10,7 @@ void FTVertexShader::CompileShader(FoxtrotRenderer* renderer)
 		return;
 	mShader.Reset();
 
-	const wchar_t* fileName = RelativePath().WC_Str();
+	const wchar_t* fileName = GetRelativePath().WC_Str();
 	DX::ThrowIfFailed(
 		D3D11Utils::CreateVertexShaderAndInputLayout(
 			renderer->GetDevice(),
@@ -20,9 +20,6 @@ void FTVertexShader::CompileShader(FoxtrotRenderer* renderer)
 			mInputLayout));
 	delete[] fileName;
 }
-
-ComPtr<ID3D11VertexShader>& FTVertexShader::GetShader() { return mShader; }
-ComPtr<ID3D11InputLayout>&	FTVertexShader::GetInputLayout() { return mInputLayout; }
 
 void FTVertexShader::RegisterInputElementDesc(const char* semanticName, UINT& offset)
 {
@@ -49,8 +46,8 @@ void FTVertexShader::RegisterInputElementDesc(const char* semanticName, UINT& of
 	mInputElements.push_back(desc);
 }
 
-FTVertexShader::FTVertexShader()
-	: FTShader()
+FTVertexShader::FTVertexShader(FTResourceDef& resDef, FoxtrotRenderer* renderer)
+	: FTShader(resDef, renderer)
 	, mIsSpine(false)
 #ifdef FOXTROT_EDITOR
 	, mSemanticsInclusion(DBG_NEW FTDS::HashMap<bool>(5))
@@ -68,27 +65,27 @@ FTVertexShader::~FTVertexShader()
 
 void FTVertexShader::SaveProperties(std::ofstream& ofs)
 {
-	FileIOHelper::BeginDataPackSave(ofs, ChunkKey::FT_VERTEX_SHADER);
+	FileIOHelper::BeginDataPackSave(ofs, ChunkKey::FTVertexShader::FT_VERTEX_SHADER);
 	FTResource::SaveProperties(ofs);
 
 	for (size_t i = 0; i < mSemanticsName.size(); ++i)
 	{
-		FTDS::String key = FTDS::String(ChunkKey::INPUT_ELEMENTS);
+		FTDS::String key = FTDS::String(ChunkKey::FTVertexShader::INPUT_ELEMENTS);
 		key.Append(" ");
 		key.Append(std::to_string(i).c_str());
 		FileIOHelper::SaveString(ofs, key.C_Str(), mSemanticsName.at(i));
 	}
 
-	FileIOHelper::SaveSize(ofs, ChunkKey::INPUT_ELEMENTS_COUNT, mSemanticsName.size());
+	FileIOHelper::SaveSize(ofs, ChunkKey::FTVertexShader::INPUT_ELEMENTS_COUNT, mSemanticsName.size());
 
-	FileIOHelper::SaveBool(ofs, ChunkKey::IS_SPINE_SHADER, mIsSpine);
+	FileIOHelper::SaveBool(ofs, ChunkKey::FTVertexShader::IS_SPINE_SHADER, mIsSpine);
 
-	FileIOHelper::EndDataPackSave(ofs, ChunkKey::FT_VERTEX_SHADER);
+	FileIOHelper::EndDataPackSave(ofs, ChunkKey::FTVertexShader::FT_VERTEX_SHADER);
 }
 
 void FTVertexShader::LoadProperties(std::ifstream& ifs)
 {
-	FileIOHelper::BeginDataPackLoad(ifs, ChunkKey::FT_VERTEX_SHADER);
+	FileIOHelper::BeginDataPackLoad(ifs, ChunkKey::FTVertexShader::FT_VERTEX_SHADER);
 
 	FileIOHelper::LoadBool(ifs, mIsSpine);
 
@@ -169,8 +166,7 @@ void FTVertexShader::UpdateUI()
 				mInputElements.at(i).InputSlot = i;
 		}
 
-		SaveMetaFile();
-		CompileShader(GetRenderer());
+		SaveMetaData();
 	}
 }
 	#endif
--- FoxtrotEngine/src/ResourceSystem/FTShaders/FTVertexShader.h
+++ FoxtrotEngine/src/ResourceSystem/FTShaders/FTVertexShader.h
@@ -1,51 +1,82 @@
+// ----------------------------------------------------------------
+// Foxtrot Engine 2D
+// Copyright (C) 2025 JungBae Park. All rights reserved.
+//
+// Released under the GNU General Public License v3.0
+// See LICENSE in root directory for full details.
+// ----------------------------------------------------------------
+
 #pragma once
-#include "ResourceSystem/FTShaders/FTShader.h"
+#include <ResourceSystem/FTShaders/FTShader.h>
 
 #include <vector>
 
 #include "Static/HashMap.h"
 
+/// @brief A wrapper for HLSL vertex Shaders.
 class FTVertexShader : public FTShader
 {
 public:
-	// You may use this overriden menber function.
-	void CompileShader(FoxtrotRenderer* renderer) override;
+	/// @see FTResource::SaveProperties()
+	virtual void SaveProperties(std::ofstream& ofs) override;
+
+	/// @see FTResource::LoadProperties()
+	virtual void LoadProperties(std::ifstream& ifs) override;
 
 public:
+	/// @brief Returns compiled HLSL shader object.
 	ComPtr<ID3D11VertexShader>& GetShader();
-	ComPtr<ID3D11InputLayout>&	GetInputLayout();
 
-	void RegisterInputElementDesc(const char* semanticName, UINT& offset);
+	/// @brief Returns the current input layout.
+	ComPtr<ID3D11InputLayout>& GetInputLayout();
 
 public:
-	FTVertexShader();
+	/// @see FTShader::FTShader()
+	FTVertexShader(FTResourceDef& resDef, FoxtrotRenderer* renderer);
 	~FTVertexShader();
 
+protected:
+	/// @see FTShader::CompileShader()
+	void CompileShader(FoxtrotRenderer* renderer) override;
+
 private:
+	/// @brief The shader should remain compiled after initialization.
 	ComPtr<ID3D11VertexShader> mShader;
-	ComPtr<ID3D11InputLayout>  mInputLayout;
 
+	ComPtr<ID3D11InputLayout> mInputLayout;
+
+	/// @brief Input elements use to compile vertex shader.
 	std::vector<D3D11_INPUT_ELEMENT_DESC> mInputElements;
-	std::vector<FTDS::String>			  mSemanticsName;
-	bool								  mIsSpine;
 
-public:
-	virtual void SaveProperties(std::ofstream& ofs) override;
-	virtual void LoadProperties(std::ifstream& ifs) override;
+	/// @brief These strings will be combined with D3D11_INPUT_ELEMENT_DESC when compiling the vertex shader.
+	std::vector<FTDS::String> mSemanticsName;
+
+	/// @brief Is this vertex shader used for rendering spine animation?
+	/// This should be defined due to the difference in rendering between Mesh and SpineMesh.
+	bool mIsSpine;
+
+private:
+	/// @brief Resgister new D3D11_INPUT_ELEMENT_DESC to mInputElements
+	void RegisterInputElementDesc(const char* semanticName, UINT& offset);
 
 #ifdef FOXTROT_EDITOR
 public:
 	void UpdateUI() override;
 
 private:
+	/// @brief Used to register input elements with GUI.
 	FTDS::HashMap<bool>* mSemanticsInclusion;
 #endif
 };
 
 namespace ChunkKey
 {
-	constexpr const char* FT_VERTEX_SHADER	   = "FTVertexShader";
-	constexpr const char* INPUT_ELEMENTS_COUNT = "Input Elements Count";
-	constexpr const char* INPUT_ELEMENTS	   = "Input Elements";
-	constexpr const char* IS_SPINE_SHADER	   = "Is Spine Shader";
+	namespace FTVertexShader
+	{
+		constexpr const char* FT_VERTEX_SHADER	   = "FTVertexShader";
+		constexpr const char* INPUT_ELEMENTS_COUNT = "Input Elements Count";
+		constexpr const char* INPUT_ELEMENTS	   = "Input Elements";
+		constexpr const char* IS_SPINE_SHADER	   = "Is Spine Shader";
+
+	} // namespace FTVertexShader
 } // namespace ChunkKey

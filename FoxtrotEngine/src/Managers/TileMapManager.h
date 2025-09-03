ad3537c6c8fc85b65063131b6a8d0ffe50ad1c66
Joseph Park Dev <human1654@gmail.com>
2025-09-04 07:15:34 +0900

    REMOVED TileMapManager: It is not necessary, since the spritesheet is removed

--- FoxtrotEngine/src/Managers/TileMapManager.h
+++ FoxtrotEngine/src/Managers/TileMapManager.h
@@ -1,20 +1,20 @@
-#pragma once
-#include "Core/SingletonMacro.h"
-
-#include <Windows.h>
-
-class FTTileMap;
-
-class TileMapManager
-{
-	SINGLETON(TileMapManager)
-
-private:
-	void SaveTileMapAsFile(FTTileMap* tileMap);
-	void SaveSpriteSheetAsFile(FTSpriteSheet* tileMap);
-
-#ifdef FOXTROT_EDITOR
-public:
-	void UpdateUI(bool* opened);
-#endif
-};
+//#pragma once
+//#include "Core/SingletonMacro.h"
+//
+//#include <Windows.h>
+//
+//class FTTileMap;
+//
+//class TileMapManager
+//{
+//	SINGLETON(TileMapManager)
+//
+//private:
+//	void SaveTileMapAsFile(FTTileMap* tileMap);
+//	//void SaveSpriteSheetAsFile(FTSpriteSheet* tileMap);
+//
+//#ifdef FOXTROT_EDITOR
+//public:
+//	void UpdateUI(bool* opened);
+//#endif
+//};

834cfa2f7dedc9178236867e57e2874dc212e00e
Joseph Park Dev <human1654@gmail.com>
2025-09-03 10:49:48 +0900

    COMMIT FTPixelShader: Added comments

--- FoxtrotEngine/src/ResourceSystem/FTShaders/FTPixelShader.cpp
+++ FoxtrotEngine/src/ResourceSystem/FTShaders/FTPixelShader.cpp
@@ -3,36 +3,37 @@
 #include "Renderer/FoxtrotRenderer.h"
 #include "Renderer/D3D11Utils.h"
 
+void FTPixelShader::SaveProperties(std::ofstream& ofs)
+{
+	FileIOHelper::BeginDataPackSave(ofs, ChunkKey::FTPixelShader::FT_PIXEL_SHADER);
+	FTResource::SaveProperties(ofs);
+	FileIOHelper::EndDataPackSave(ofs, ChunkKey::FTPixelShader::FT_PIXEL_SHADER);
+}
+
+void FTPixelShader::LoadProperties(std::ifstream& ifs)
+{
+	FileIOHelper::BeginDataPackLoad(ifs, ChunkKey::FTPixelShader::FT_PIXEL_SHADER);
+	FTResource::LoadProperties(ifs);
+}
+
+ComPtr<ID3D11PixelShader>& FTPixelShader::GetShader() { return mShader; }
+
+FTPixelShader::FTPixelShader(FTResourceDef& resDef, FoxtrotRenderer* renderer)
+	: FTShader(resDef, renderer)
+{
+	SetType(ShaderType::PIXEL_SHADER);
+}
+
 void FTPixelShader::CompileShader(FoxtrotRenderer* renderer)
 {
 	mShader.Reset();
 
-	const wchar_t* fileName = RelativePath().WC_Str();
+	const wchar_t* fileName = GetRelativePath().WC_Str();
 	D3D11Utils::CreatePixelShader(
 		renderer->GetDevice(),
 		fileName,
 		mShader);
 	delete[] fileName;
-}
-
-ComPtr<ID3D11PixelShader>& FTPixelShader::GetShader() { return mShader; }
-
-FTPixelShader::FTPixelShader()
-{
-	SetType(ShaderType::PIXEL_SHADER);
-}
-
-void FTPixelShader::SaveProperties(std::ofstream& ofs)
-{
-	FileIOHelper::BeginDataPackSave(ofs, ChunkKey::FT_PIXEL_SHADER);
-	FTResource::SaveProperties(ofs);
-	FileIOHelper::EndDataPackSave(ofs, ChunkKey::FT_PIXEL_SHADER);
-}
-
-void FTPixelShader::LoadProperties(std::ifstream& ifs)
-{
-	FileIOHelper::BeginDataPackLoad(ifs, ChunkKey::FT_PIXEL_SHADER);
-	FTResource::LoadProperties(ifs);
 }
 
 #ifdef FOXTROT_EDITOR
--- FoxtrotEngine/src/ResourceSystem/FTShaders/FTPixelShader.h
+++ FoxtrotEngine/src/ResourceSystem/FTShaders/FTPixelShader.h
@@ -1,25 +1,40 @@
-#pragma once
-#include "ResourceSystem/FTShaders/FTShader.h"
+// ----------------------------------------------------------------
+// Foxtrot Engine 2D
+// Copyright (C) 2025 JungBae Park. All rights reserved.
+//
+// Released under the GNU General Public License v3.0
+// See LICENSE in root directory for full details.
+// ----------------------------------------------------------------
 
+#pragma once
+#include <ResourceSystem/FTShaders/FTShader.h>
+
+/// @brief A wrapper for HLSL pixel shaders.
 class FTPixelShader :
 	public FTShader
 {
 public:
-	// You may use this overriden menber function.
-	void CompileShader(FoxtrotRenderer* renderer) override;
+	/// @see FTResource::SaveProperties()
+	virtual void SaveProperties(std::ofstream& ofs) override;
+
+	/// @see FTResource::LoadProperties()
+	virtual void LoadProperties(std::ifstream& ifs) override;
 
 public:
+	/// @brief Returns compiled HLSL pixel shader object.
 	ComPtr<ID3D11PixelShader>& GetShader();
 
 public:
-	FTPixelShader();
+	/// @see FTShader::FTShader()
+	FTPixelShader(FTResourceDef& resDef, FoxtrotRenderer* renderer);
+
+protected:
+	/// @see FTShader::CompileShader()
+	void CompileShader(FoxtrotRenderer* renderer) override;
 
 private:
+	/// @brief The shader should remain compiled after initialization.
 	ComPtr<ID3D11PixelShader> mShader;
-
-public:
-	virtual void SaveProperties(std::ofstream& ofs) override;
-	virtual void LoadProperties(std::ifstream& ifs) override;
 
 #ifdef FOXTROT_EDITOR
 public:
@@ -29,5 +44,8 @@ public:
 
 namespace ChunkKey
 {
-	constexpr const char* FT_PIXEL_SHADER = "FTPixelShader";
-}
+	namespace FTPixelShader
+	{
+		constexpr const char* FT_PIXEL_SHADER = "FTPixelShader";
+	}
+} // namespace ChunkKey

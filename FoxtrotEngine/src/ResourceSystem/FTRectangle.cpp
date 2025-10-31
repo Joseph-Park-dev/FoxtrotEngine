bf7f772a1eaf1599ff2ff5741e0d2881e04284ad
Joseph Park Dev <human1654@gmail.com>
2025-10-31 19:32:10 +0900

    COMMIT FTRectangle.cpp: Revised Initialize() member function, it uses FTVector4 to generate debug pont

--- FoxtrotEngine/src/ResourceSystem/FTRectangle.cpp
+++ FoxtrotEngine/src/ResourceSystem/FTRectangle.cpp
@@ -10,12 +10,12 @@ using Matrix = DirectX::SimpleMath::Matrix;
 
 void FTRectangle::Initialize(FoxtrotRenderer* renderer)
 {
-    FTShape::Initialize(renderer);
-    InitializeMesh(renderer->GetDevice(), GeometryGenerator::MakeDebugPoint(FTVector3(1.f,0.f,0.f)));
+	FTShape::Initialize(renderer);
+	InitializeMesh(renderer->GetDevice(), GeometryGenerator::MakeDebugPoint(FTVector4(1.f, 0.f, 0.f, 1.f)));
 }
 
 FTRectangle::FTRectangle()
-    : FTShape()
+	: FTShape()
 {
 }
 
@@ -25,26 +25,26 @@ FTRectangle::~FTRectangle()
 
 void FTRectangle::SaveProperties(std::ofstream& ofs)
 {
-    FileIOHelper::BeginDataPackSave(ofs, ChunkKey::FTRECTANGLE);
-    FileIOHelper::SaveBool(ofs, ChunkKey::FTSHAPE_IS_ACTIVE, GetIsActive());
-    FileIOHelper::EndDataPackSave(ofs, ChunkKey::FTRECTANGLE);
+	FileIOHelper::BeginDataPackSave(ofs, ChunkKey::FTRECTANGLE);
+	FileIOHelper::SaveBool(ofs, ChunkKey::FTSHAPE_IS_ACTIVE, GetIsActive());
+	FileIOHelper::EndDataPackSave(ofs, ChunkKey::FTRECTANGLE);
 }
 
 void FTRectangle::LoadProperties(std::ifstream& ifs)
 {
-    bool isActive = false;
+	bool isActive = false;
 
-    FileIOHelper::BeginDataPackLoad(ifs, ChunkKey::FTRECTANGLE);
-    FileIOHelper::LoadBool(ifs, isActive);
+	FileIOHelper::BeginDataPackLoad(ifs, ChunkKey::FTRECTANGLE);
+	FileIOHelper::LoadBool(ifs, isActive);
 
-    SetIsActive(isActive);
+	SetIsActive(isActive);
 }
 
 #ifdef FOXTROT_EDITOR
 void FTRectangle::UpdateUI()
 {
-    bool isActive = GetIsActive();
-    CommandHistory::GetInstance()->UpdateBoolValue("Render Shape", isActive);
-    SetIsActive(isActive);
+	bool isActive = GetIsActive();
+	CommandHistory::GetInstance()->UpdateBoolValue("Render Shape", isActive);
+	SetIsActive(isActive);
 }
 #endif

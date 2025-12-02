747714f985e364824a683407e0714ac92c2d5175
jbpark1654 <jbpark1654@gmail.com>
2025-12-02 17:32:29 +0900

    COMMIT HeapNode.h : Revised Constructor to avoid unnecessary value copy

--- FoxtrotDS/Node/HeapNode.h
+++ FoxtrotDS/Node/HeapNode.h
@@ -1,11 +1,18 @@
-#pragma once
+// ----------------------------------------------------------------
+// Foxtrot Engine 2D
+// Copyright (C) 2025 JungBae Park. All rights reserved.
+//
+// Released under the GNU General Public License v3.0
+// See LICENSE in root directory for full details.
+// ----------------------------------------------------------------
 
+#pragma once
 namespace FTDS
 {
 	template <typename TYPE>
 	struct HeapNode
 	{
-		HeapNode(size_t key, TYPE value)
+		HeapNode(size_t key, const TYPE& value)
 			: Key(key), Value(value)
 		{
 		}

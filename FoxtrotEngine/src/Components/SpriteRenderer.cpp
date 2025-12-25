56e1ae4ae548432c13191ff6125cf46dfb63cbde
Joseph Park Dev <human1654@gmail.com>
2025-12-25 16:23:35 +0900

    COMMIT SpriteRenderer (.h, .cpp) : Integrated FTSprite

--- FoxtrotEngine/src/Components/SpriteRenderer.cpp
+++ FoxtrotEngine/src/Components/SpriteRenderer.cpp
@@ -19,10 +19,12 @@
 #include "Renderer/D3D11Utils.h"
 #include "Renderer/FoxtrotRenderer.h"
 #include "ResourceSystem/FTMeshGroup.h"
+#include "ResourceSystem/FTSprite/FTSprite.h"
 #include "ResourceSystem/FTTexture.h"
 #include "ResourceSystem/GeometryGenerator.h"
 #include "ResourceSystem/Mesh.h"
 #include "ResourceSystem/FTMeshData.h"
+#include "ResourceSystem/FTMaterials/FTMaterial.h"
 #include "Scenes/Scene.h"
 
 #ifdef FOXTROT_EDITOR
@@ -37,45 +39,114 @@
 
 void SpriteRenderer::Initialize(FTCore* coreInstance)
 {
-	MeshRenderer::Initialize(coreInstance);
-	FTDS::String key	= ChunkKey::PRIMITIVE_SQUARE_SPRITE;
-	FTMeshGroup* square = ResourceManager::GetInstance()->GetLoadedMesh(key);
-	SetMeshGroup(square);
+	Component::Initialize(coreInstance);
+}
+
+void SpriteRenderer::Render(FoxtrotRenderer* renderer)
+{
+	if (mSprite)
+		mSprite->Render(renderer, GetOwner()->GetTransform(), Camera::GetInstance(), mVS, mGS, mPS, mMaterial);
 }
 
 void SpriteRenderer::CloneTo(Actor* actor)
 {
 	SpriteRenderer* newComp = DBG_NEW SpriteRenderer(actor, GetUpdateOrder());
-	newComp->SetRenderer(this->GetRenderer());
-	newComp->SetMeshGroup(this->GetMeshGroup());
-	newComp->SetTexture(this->GetTexture());
-	newComp->SetVS(this->GetVS());
-	newComp->SetPS(this->GetPS());
-	newComp->SetMaterial(this->GetMaterial());
-
-	// newComp->GetMeshGroup()->SetDrawNormal(this->GetMeshGroup()->GetDrawNormal());
-	newComp->mChannel  = this->mChannel;
+	newComp->mSprite		= this->mSprite;
+	newComp->mVS			= this->mVS;
+	newComp->mGS			= this->mGS;
+	newComp->mPS			= this->mPS;
+	newComp->mMaterial		= this->mMaterial;
 }
 
 SpriteRenderer::SpriteRenderer(Actor* owner, int updateOrder)
-	: MeshRenderer(owner, updateOrder)
-	, mChannel(4)
+	: Component(owner, updateOrder)
+	, mSprite(nullptr)
+	, mMaterial(nullptr)
 {
+	FTDS::String key = Path::SpriteRenderer::VS;
+	mVS				 = ResourceManager::GetInstance()->GetLoadedVertexShader(key);
+
+	key = Path::SpriteRenderer::GS;
+	mGS = ResourceManager::GetInstance()->GetLoadedGeometryShader(key);
+
+	key = Path::SpriteRenderer::PS;
+	mPS = ResourceManager::GetInstance()->GetLoadedPixelShader(key);
 }
+
+FTSprite* SpriteRenderer::GetSprite() const
+{
+	return mSprite;
+}
+
+void SpriteRenderer::SetSprite(FTSprite* sprite)
+{
+	mSprite = sprite;
+}
+
+FTVertexShader* SpriteRenderer::GetVS() const
+{
+	return mVS;
+}
+
+FTGeometryShader* SpriteRenderer::GetGS() const
+{
+	return mGS;
+}
+
+FTPixelShader* SpriteRenderer::GetPS() const
+{
+	return mPS;
+}
+
+FTMaterial* SpriteRenderer::GetMaterial() const
+{
+	return mMaterial;
+}
+
+void SpriteRenderer::SetVS(FTVertexShader* vs) { mVS = vs; }
+void SpriteRenderer::SetGS(FTGeometryShader* gs) { mGS = gs; }
+void SpriteRenderer::SetPS(FTPixelShader* ps) { mPS = ps; }
+void SpriteRenderer::SetMaterial(FTMaterial* mat) { mMaterial = mat; }
 
 void SpriteRenderer::SaveProperties(std::ofstream& ofs)
 {
-	MeshRenderer::SaveProperties(ofs);
+	Component::SaveProperties(ofs);
+	FileIOHelper::SaveString(ofs, ChunkKey::SpriteRenderer::SPRITE, mSprite->GetFileName());
+	FileIOHelper::SaveString(ofs, ChunkKey::SpriteRenderer::MATERIAL, mMaterial->GetFileName());
 }
 
 void SpriteRenderer::LoadProperties(std::ifstream& ifs)
 {
-	MeshRenderer::LoadProperties(ifs);
+	FTDS::String matKey;
+	FileIOHelper::LoadBasicString(ifs, matKey);
+
+	FTDS::String spriteKey;
+	FileIOHelper::LoadBasicString(ifs, spriteKey);
+	Component::LoadProperties(ifs);
+
+	mSprite	  = ResourceManager::GetInstance()->GetLoadedSprite(spriteKey);
+	mMaterial = ResourceManager::GetInstance()->GetLoadedMaterial(matKey);
 }
 
 #ifdef FOXTROT_EDITOR
 void SpriteRenderer::EditorUIUpdate()
 {
-	MeshRenderer::EditorUIUpdate();
+	Component::EditorUIUpdate();
+
+	if (mSprite)
+		mSprite->UpdateUI();
+
+	FTEditorUtils::DisplayResSelection(
+		"Select Sprite",
+		ResourceManager::GetInstance()->GetSprites(),
+		mSprite);
+
+	if (mMaterial)
+		mMaterial->UpdateUI();
+
+	FTEditorUtils::DisplayResSelection(
+		"Select Material",
+		ResourceManager::GetInstance()->GetMaterials(),
+		mMaterial);
 }
 #endif // FOXTROT_EDITOR

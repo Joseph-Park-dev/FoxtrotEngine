bd297850ea5a055f4acd113da89a960e6aa2e840
JungBae Park <jbpark1654@gmail.com>
2026-02-24 15:45:47 +0900

    COMMIT SpritePS.hlsl : Revised shader

--- FoxtrotEditor/FoxtrotEngine/Assets/Shaders/2D/SpritePS.hlsl
+++ FoxtrotEditor/FoxtrotEngine/Assets/Shaders/2D/SpritePS.hlsl
@@ -13,17 +13,16 @@ SamplerState g_sampler : register(s0);
 
 cbuffer PSCBuf : register(b0)
 {
-    float4 color;
     bool useTexture;
-    bool3 dummy;
+    float alphaTrim;
+    float2 dummy;
+    float4 color;
 };
 
 float4 main(PS_IN input) : SV_Target
 {
-    float alphaThres = 0.05;
-    
     float4 result = useTexture ? g_texture0.Sample(g_sampler, input.texCoord) * color : color;
-    if (result.w < alphaThres)
+    if (result.w < alphaTrim)
         clip(-1); // Discards the pixel
     
     return result;

f5ba5d5d91c69b7ceb4ffe4052c108d223f237f9
jbpark1654 <jbpark1654@gmail.com>
2026-05-06 13:56:28 +0900

    ADDED Installation scripts

---
+++ script/clone_deps.bat
@@ -0,0 +1,118 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: ============================================================
+:: Configuration
+:: ============================================================
+set BASE_DIR=%~dp0..\extern
+set GIT=git
+
+:: ============================================================
+:: Repositories to clone
+:: ============================================================
+set REPOS[0]=https://github.com/ocornut/imgui.git
+set REPOS[1]=https://github.com/nlohmann/json.git
+
+:: ============================================================
+:: Clone loop
+:: ============================================================
+if not exist "%BASE_DIR%" mkdir "%BASE_DIR%"
+
+call :clone "!REPOS[0]!" "docking"
+call :clone "!REPOS[1]!"
+
+echo.
+echo All done.
+exit /b 0
+
+:: ============================================================
+:: :clone subroutine
+:: ============================================================
+:clone
+set REPO_URL=%~1
+set BRANCH=%~2
+for %%F in (%REPO_URL%) do set REPO_NAME=%%~nF
+
+echo [*] Cloning %REPO_NAME% from %REPO_URL%...
+
+if exist "%BASE_DIR%\%REPO_NAME%" (
+    echo [!] Already exists, skipping: %REPO_NAME%
+) else (
+    if not "%BRANCH%"=="" (
+        %GIT% clone -b "%BRANCH%" "%REPO_URL%" "%BASE_DIR%\%REPO_NAME%"
+    ) else (
+        %GIT% clone "%REPO_URL%" "%BASE_DIR%\%REPO_NAME%"
+    )
+    if errorlevel 1 (
+        echo [ERROR] Failed to clone %REPO_NAME%
+    ) else (
+        echo [OK] Cloned %REPO_NAME%
+        if not "%BRANCH%"=="" echo [OK] Checked out branch: %BRANCH%
+    )
+)
+exit /b
+
+:: ============================================================
+:: Configuration
+:: ============================================================
+set VCXPROJ=src\Core\Core.vcxproj
+
+:: Source files (.cpp) to add
+set SOURCES[0]=extern\imgui\imgui.cpp
+set SOURCES[1]=extern\imgui\imgui_draw.cpp
+set SOURCES[1]=extern\imgui\imgui_tables.cpp
+set SOURCES[1]=extern\imgui\imgui_widgetscpp
+
+:: ============================================================
+:: Build PowerShell argument lists
+:: ============================================================
+set SRC_LIST=
+for /L %%i in (0,1,1) do (
+    set SRC_LIST=!SRC_LIST!'!SOURCES[%%i]!',
+)
+set SRC_LIST=!SRC_LIST:~0,-1!
+
+set HDR_LIST=
+for /L %%i in (0,1,1) do (
+    set HDR_LIST=!HDR_LIST!'!HEADERS[%%i]!',
+)
+set HDR_LIST=!HDR_LIST:~0,-1!
+
+:: ============================================================
+:: Invoke PowerShell to patch the .vcxproj
+:: ============================================================
+powershell -NoProfile -ExecutionPolicy Bypass -Command ^
+    "$proj = '%VCXPROJ%';" ^
+    "$sources = @(%SRC_LIST%);" ^
+    "$headers = @(%HDR_LIST%);" ^
+    "[xml]$xml = Get-Content $proj;" ^
+    "$ns = 'http://schemas.microsoft.com/developer/msbuild/2003';" ^
+    "$nsmgr = New-Object System.Xml.XmlNamespaceManager($xml.NameTable);" ^
+    "$nsmgr.AddNamespace('ms', $ns);" ^
+    "$srcGroup = $xml.SelectSingleNode('//ms:ItemGroup[ms:ClCompile]', $nsmgr);" ^
+    "if (-not $srcGroup) { $srcGroup = $xml.CreateElement('ItemGroup', $ns); $xml.Project.AppendChild($srcGroup) | Out-Null; }" ^
+    "foreach ($f in $sources) {" ^
+    "  $existing = $srcGroup.SelectSingleNode(\"ms:ClCompile[@Include='$f']\", $nsmgr);" ^
+    "  if (-not $existing) {" ^
+    "    $el = $xml.CreateElement('ClCompile', $ns);" ^
+    "    $el.SetAttribute('Include', $f);" ^
+    "    $srcGroup.AppendChild($el) | Out-Null;" ^
+    "    Write-Host \"[OK] Added source: $f\";" ^
+    "  } else { Write-Host \"[!] Already present: $f\"; }" ^
+    "}" ^
+    "$hdrGroup = $xml.SelectSingleNode('//ms:ItemGroup[ms:ClInclude]', $nsmgr);" ^
+    "if (-not $hdrGroup) { $hdrGroup = $xml.CreateElement('ItemGroup', $ns); $xml.Project.AppendChild($hdrGroup) | Out-Null; }" ^
+    "foreach ($f in $headers) {" ^
+    "  $existing = $hdrGroup.SelectSingleNode(\"ms:ClInclude[@Include='$f']\", $nsmgr);" ^
+    "  if (-not $existing) {" ^
+    "    $el = $xml.CreateElement('ClInclude', $ns);" ^
+    "    $el.SetAttribute('Include', $f);" ^
+    "    $hdrGroup.AppendChild($el) | Out-Null;" ^
+    "    Write-Host \"[OK] Added header: $f\";" ^
+    "  } else { Write-Host \"[!] Already present: $f\"; }" ^
+    "}" ^
+    "$xml.Save((Resolve-Path $proj));"
+
+echo.
+echo Done patching %VCXPROJ%.
+pause
---
+++ script/patch_vcxproj-Core.bat
@@ -0,0 +1,83 @@
+@echo off
+setlocal enabledelayedexpansion
+:: ============================================================
+:: Configuration
+:: ============================================================
+set VCXPROJ=..\src\Core\Core.vcxproj
+set PS_TEMP=%~dp0patch_vcxproj_temp.ps1
+
+:: ============================================================
+:: Source files (.cpp) to add
+:: ============================================================
+set SOURCES[0]=extern\imgui\imgui.cpp
+set /a SRC_COUNT+=1
+set SOURCES[1]=extern\imgui\imgui_draw.cpp
+set /a SRC_COUNT+=1
+set SOURCES[2]=extern\imgui\imgui_tables.cpp
+set /a SRC_COUNT+=1
+set SOURCES[3]=extern\imgui\imgui_widgetscpp
+set /a SRC_COUNT+=1
+
+:: ============================================================
+:: Header files (.h) to add
+:: ============================================================
+::set HDR_COUNT=0
+::set HEADERS[0]=include\foo.h
+::set /a HDR_COUNT+=1
+::set HEADERS[1]=include\bar.h
+::set /a HDR_COUNT+=1
+
+:: ============================================================
+:: Write PowerShell script to temp file
+:: ============================================================
+set /a SRC_LAST=SRC_COUNT-1
+set /a HDR_LAST=HDR_COUNT-1
+
+echo $proj = '%VCXPROJ%'                                              > "%PS_TEMP%"
+echo $sources = @(                                                    >> "%PS_TEMP%"
+for /L %%i in (0,1,!SRC_LAST!) do (
+    echo     '!SOURCES[%%i]!'                                         >> "%PS_TEMP%"
+)
+echo )                                                                >> "%PS_TEMP%"
+echo $headers = @(                                                    >> "%PS_TEMP%"
+for /L %%i in (0,1,!HDR_LAST!) do (
+    echo     '!HEADERS[%%i]!'                                         >> "%PS_TEMP%"
+)
+echo )                                                                >> "%PS_TEMP%"
+echo [xml]$xml = Get-Content $proj                                    >> "%PS_TEMP%"
+echo $ns = 'http://schemas.microsoft.com/developer/msbuild/2003'     >> "%PS_TEMP%"
+echo $nsmgr = New-Object System.Xml.XmlNamespaceManager($xml.NameTable) >> "%PS_TEMP%"
+echo $nsmgr.AddNamespace('ms', $ns)                                  >> "%PS_TEMP%"
+echo $srcGroup = $xml.SelectSingleNode('//ms:ItemGroup[ms:ClCompile]', $nsmgr) >> "%PS_TEMP%"
+echo if (-not $srcGroup) { $srcGroup = $xml.CreateElement('ItemGroup', $ns); $xml.Project.AppendChild($srcGroup) ^| Out-Null } >> "%PS_TEMP%"
+echo foreach ($f in $sources) {                                       >> "%PS_TEMP%"
+echo   $existing = $srcGroup.SelectSingleNode("ms:ClCompile[@Include='$f']", $nsmgr) >> "%PS_TEMP%"
+echo   if (-not $existing) {                                          >> "%PS_TEMP%"
+echo     $el = $xml.CreateElement('ClCompile', $ns)                  >> "%PS_TEMP%"
+echo     $el.SetAttribute('Include', $f)                             >> "%PS_TEMP%"
+echo     $srcGroup.AppendChild($el) ^| Out-Null                      >> "%PS_TEMP%"
+echo     Write-Host "[OK] Added source: $f"                          >> "%PS_TEMP%"
+echo   } else { Write-Host "[!] Already present: $f" }               >> "%PS_TEMP%"
+echo }                                                                >> "%PS_TEMP%"
+echo $hdrGroup = $xml.SelectSingleNode('//ms:ItemGroup[ms:ClInclude]', $nsmgr) >> "%PS_TEMP%"
+echo if (-not $hdrGroup) { $hdrGroup = $xml.CreateElement('ItemGroup', $ns); $xml.Project.AppendChild($hdrGroup) ^| Out-Null } >> "%PS_TEMP%"
+echo foreach ($f in $headers) {                                       >> "%PS_TEMP%"
+echo   $existing = $hdrGroup.SelectSingleNode("ms:ClInclude[@Include='$f']", $nsmgr) >> "%PS_TEMP%"
+echo   if (-not $existing) {                                          >> "%PS_TEMP%"
+echo     $el = $xml.CreateElement('ClInclude', $ns)                  >> "%PS_TEMP%"
+echo     $el.SetAttribute('Include', $f)                             >> "%PS_TEMP%"
+echo     $hdrGroup.AppendChild($el) ^| Out-Null                      >> "%PS_TEMP%"
+echo     Write-Host "[OK] Added header: $f"                          >> "%PS_TEMP%"
+echo   } else { Write-Host "[!] Already present: $f" }               >> "%PS_TEMP%"
+echo }                                                                >> "%PS_TEMP%"
+echo $xml.Save((Resolve-Path $proj))                                  >> "%PS_TEMP%"
+
+:: ============================================================
+:: Execute and clean up
+:: ============================================================
+powershell -NoProfile -ExecutionPolicy Bypass -File "%PS_TEMP%"
+del "%PS_TEMP%"
+
+echo.
+echo Done patching %VCXPROJ%.
+pause

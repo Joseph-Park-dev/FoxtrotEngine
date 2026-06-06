4a39d52a0a95a318baf744b61741240243b1ea9f
jbpark1654 <jbpark1654@gmail.com>
2026-06-06 19:06:50 +0900

    ADD GenericData: create GenericData module with FTCSV, FTJSON, FTText relocated from Core

---
+++ Engine/GenericData/GenericData.cpp
@@ -0,0 +1,22 @@
+// GenericData.cpp : Defines the exported functions for the DLL.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "GenericData.h"
+
+
+// This is an example of an exported variable
+GENERICDATA_API int nGenericData=0;
+
+// This is an example of an exported function.
+GENERICDATA_API int fnGenericData(void)
+{
+    return 0;
+}
+
+// This is the constructor of a class that has been exported.
+CGenericData::CGenericData()
+{
+    return;
+}
